8b052b706b0ce3857af3e91dea77995457b8169c
JonGoo <jonathan.goossens@hotmail.com>
2019-06-11 11:07:54 +0200

    Full maven project work

--- AlderalifeBackEnd/src/main/java/com/fivem/alderalife/model/RoleName.java
+++
@@ -1,7 +0,0 @@
-package com.fivem.alderalife.model;
-
-public enum RoleName {
-    ROLE_USER,
-    ROLE_ADMIN,
-    ROLE_MOD
-}
---
+++ alderalife-frontend/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { HomepageComponent } from './homepage/homepage.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    HomepageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
