ee9e1eed7a18b41a6f06b2ec7ae3ae3f80d3cd41
JonGoo <jonathan.goossens@hotmail.com>
2019-06-24 12:26:14 +0200

    Correction of the test to have a travis CI working

--- alderalife-backend/src/test/java/com/fivem/alderalife/AlderalifeApplicationTests.java
+++ alderalife-backend/src/test/java/com/fivem/alderalife/AlderalifeApplicationTests.java
@@ -1,13 +1,54 @@
 package com.fivem.alderalife;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class AlderalifeApplicationTests {
 
+    @Autowired
+    private TestRestTemplate restTemplate;
 
+    @LocalServerPort
+    int randomServerPort;
+
+    /**
+     * Test on mapping for the patient creation
+     * @throws URISyntaxException Exception if the URI is not working correctly
+     */
+    @Test
+    public void testRegister()throws URISyntaxException {
+        final String baseUrl = "http://localhost:"+randomServerPort+"/api/auth/signup";
+        URI uri= new URI(baseUrl);
+        String sending = "{"+
+                "\"username\": \"Goostry\"," +
+                "\"email\": \"jonathan.goossens@hotmail.com\","+
+                "\"password\": \"springtest\"" +
+                "}";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("CONTENT-TYPE", "application/json");
+
+        HttpEntity<String> request = new HttpEntity<>(sending, headers);
+
+        ResponseEntity<String> result = this.restTemplate.postForEntity(uri, request, String.class);
+
+        //Verify request succeed
+        Assert.assertEquals(201, result.getStatusCode().value());
+
+    }
 }
+
---
+++ alderalife-backend/src/test/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO `roles`  (`id`, `name`)
+SELECT * FROM (SELECT 1, 'ROLE_USER') as test WHERE NOT EXISTS(SELECT id from `roles` where `id` = 1);
+INSERT INTO `roles`  (`id`, `name`)
+SELECT * FROM (SELECT 2, 'ROLE_ADMIN') as test WHERE NOT EXISTS(SELECT id from `roles` where `id` = 2);
+INSERT INTO `roles`  (`id`, `name`)
+SELECT * FROM (SELECT 3, 'ROLE_MOD') as test WHERE NOT EXISTS(SELECT id from `roles` where `id` = 3);
